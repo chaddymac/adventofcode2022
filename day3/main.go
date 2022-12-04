00ddfa279321b59aa8b4ac8aabe9ddb274c87f7c
Cha'Diamond Moody <chadiamondmoody@chadiamonds-air.lan>
2022-12-03 23:45:16 -0500

    day3 complete

--- day3/main.go
+++ day3/main.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var compartmentOne []string
 var compartmentTwo []string
 var score int
+var groupOne []string
+var groupTwo []string
+var groupThree []string
+var letter string
 
 // one item per rucksack is wrong
 // input is the items in each rucksack
@@ -18,7 +23,8 @@ var score int
 // 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s);
 
 func main() {
-	fmt.Println(rucksack("input.txt"))
+	// fmt.Println(rucksack("input.txt"))
+	fmt.Println(rucksackGroup("input.txt"))
 }
 
 func rucksack(input string) int {
@@ -38,21 +44,69 @@ func rucksack(input string) int {
 	}
 
 	for i := 0; i < len(compartmentOne); i++ {
-		// utf8.RuneCountInString(str)
-		if strings.Contains(compartmentTwo[i], "p") && strings.Contains(compartmentOne[i], "p") {
-			score += 16
-		} else if strings.Contains(compartmentTwo[i], "L") && strings.Contains(compartmentOne[i], "L") {
-			score += 38
-		} else if strings.Contains(compartmentTwo[i], "P") && strings.Contains(compartmentOne[i], "P") {
-			score += 42
-		} else if strings.Contains(compartmentTwo[i], "v") && strings.Contains(compartmentOne[i], "v") {
-			score += 22
-		} else if strings.Contains(compartmentTwo[i], "t") && strings.Contains(compartmentOne[i], "t") {
-			score += 20
-		} else if strings.Contains(compartmentTwo[i], "s") && strings.Contains(compartmentOne[i], "s") {
-			score += 19
+		val := findingMatch(compartmentOne[i], compartmentTwo[i])
+		a := string(val[0])
+		b, _ := utf8.DecodeRuneInString(a)
+		if unicode.IsUpper(b) {
+			score += int(b) - 38
+		} else {
+			score += int(b) - 96
 		}
 	}
 	return score
 
+}
+
+func findingMatch(comp1 string, comp2 string) string {
+	for i := 0; i < len(comp1); i++ {
+		for j := 0; j < len(comp2); j++ {
+			if comp1[i] == comp2[j] {
+				return comp1[i:]
+			}
+		}
+	}
+	return ""
+}
+
+func rucksackGroup(input string) int {
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		y := scanner.Text()
+
+		groupOne = append(groupOne, y)
+	}
+
+	for i := 0; i < len(groupOne); i += 3 {
+		firstItem := groupOne[i]
+		secondItem := groupOne[i+1]
+		thirdItem := groupOne[i+2]
+		letter = findMatchTwo(firstItem, secondItem, thirdItem)
+		b, _ := utf8.DecodeRuneInString(letter)
+		if unicode.IsUpper(b) {
+			score += int(b) - 38
+		} else {
+			score += int(b) - 96
+		}
+
+	}
+	return score
+}
+
+func findMatchTwo(comp1 string, comp2 string, comp3 string) string {
+	for i := 0; i < len(comp1); i++ {
+		for j := 0; j < len(comp2); j++ {
+			for k := 0; k < len(comp3); k++ {
+				if comp1[i] == comp2[j] && comp1[i] == comp3[k] {
+					letter = string(comp1[i])
+					return letter
+				}
+			}
+		}
+	}
+	return ""
 }
